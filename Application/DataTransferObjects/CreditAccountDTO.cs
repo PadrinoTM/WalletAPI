3ab7a1638a3c68ca248a64483b3f51bb479c1967
PadrinoTM <tmidowu@gmail.com>
2022-11-11 22:01:22 +0100

    WalletAPI Models using Clean Architecture

---
+++ Application/DataTransferObjects/CreditAccountDTO.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DataTransferObjects
+{
+    public class CreditAccountDTO
+    {
+        public string WalletNumber { get; set; }
+        public long Amount { get; set; }    
+        public WalletType Currency { get; set; }
+    }
+}
---
+++ Persistence/Repositories/AccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using Application.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class AccountRepository
+    {
+        public bool CreditAccount(CreditAccountDTO)
+        {
+
+        }
+    }
+}
+s
